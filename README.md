17ce43363f8a8bcb4e02cba6069417858de3a2e2
pete-rai <admin@cityverve.org.uk>
2018-02-08 16:14:03 +0000

    The first commit of this CityVerve demo app

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # ordnance-survey-maps-demo
-A demo of using the CityVerver API with Ordnance Survey Maps
+
+This is a demonstration of using the CityVerve API together with Ordnance Survey Maps
+
+Click [here](https://cityverve.org.uk/what-is-cityverve/) to learn more about CityVerve.
+
+Click [here](http://developer.cityverve.org.uk/) to go to the CityVerve Developer Portal.
+
+Click [here](http://developer.cityverve.org.uk/showcase) to see a showcase of working CityVerve apps (including this demo).
+
+Click [here](https://developer.ordnancesurvey.co.uk/) to go to the Ordnance Survey developer portal.
+
+## Instructions
+
+In order to get this demo working for yourself, you simply need to clone the repo and host the files within your web-server of choice.
+
+Before it will work, however, you need to make **two modification**:
+
+### CityVerve API Key
+
+At the top of the JavaScript file, you will find the following line of code:
+
+```javascript
+const API_KEY = 'YOUR-KEY-HERE';  // cityverve api key
+```
+
+You need to replace the string **YOUR-KEY-HERE** with your own personal _CityVerve API key_. You can get a key (for free) at our [Developer Portal](http://developer.cityverve.org.uk/home). Just follow the steps in our [Getting Started](http://developer.cityverve.org.uk/get-started) guide.
+
+### Ordnance Survey Maps API Key
+
+Towards the middle of the JavaScript file, you will find the following line of code:
+
+```html
+L.tileLayer ('https://api2.ordnancesurvey.co.uk/mapping_api/v1/service/zxy/EPSG%3A3857/Outdoor%203857/{z}/{x}/{y}.png?key=YOUR-KEY-HERE').addTo (map);
+```
+
+You need to replace the string **YOUR-KEY-HERE** with your own personal _Ordnance Survey Maps API key_. You can get a key at the [Ordnance Survey Developer Portal](https://developer.ordnancesurvey.co.uk/).
+
+
+_Have fun, and [please do share](https://cityverve.org.uk/contact/) any cool apps that you make with the CityVerve API._
---
+++ os-maps.js
@@ -0,0 +1,134 @@
+/*
+  CityVerve - Ordinance Survey Maps Demo
+  http://developer.cityverve.org.uk
+  https://github.com/cityverve/ordnance-survey-maps-demo
+
+  Released under the MIT license
+  https://en.wikipedia.org/wiki/MIT_License
+
+  Copyright 2018 CityVerve  */
+
+// --- constants
+
+const MANCHESTER   = { center: { lat: 53.4791595, lng: -2.2462938 }, zoom: 12 };  // Town Hall is map center point and zoom
+const API_ENDPOINT = 'http://api.cityverve.org.uk/v1/entity/';  // keep trailing slash
+const API_KEY      = 'YOUR-KEY-HERE';  // cityverve api key
+
+// --- a generic method for make an asyncronous call to an http endpoint
+
+function httpGet (url, callback)
+{
+    var xhr = new XMLHttpRequest ();
+
+    xhr.onreadystatechange = function ()
+    {
+        if (this.readyState == 4 && this.status == 200)  // readyState 4 = DONE, status 200 = HTTP/OK
+        {
+            callback (JSON.parse (this.responseText));
+        }
+    };
+
+    xhr.open ('GET', url, true);
+    xhr.setRequestHeader ('Authorization', API_KEY);
+    xhr.send ();
+
+    return xhr.responseText;
+}
+
+// --- info box for blue plaques items
+
+function infoBluePlaque (item)
+{
+    var location = item.entity.location ? "<b>" + item.entity.location + "</b> - " : "";  // sometimes blank
+
+    return '<h3>' + item.entity.recipient + '</h3>' +
+           "<p style='font-size: 1.1em;' >" + location + item.entity.text + '</p>' +
+           "<a href='" + item.entity.reference + "'>Wikipedia</a>";
+}
+
+// --- info box for history items
+
+function infoHistory (item)
+{
+    return '<h3>' + item.name + '</h3>' +
+           "<img width='250' style='border: 1px solid grey; float:left; margin-right: 12px; margin-bottom: 4px;' src='" + item.entity.image + "'/>" +
+           "<p style='font-size: 1.1em;' ><b>" + item.entity.date + "</b> - " + item.entity.description + '</p>';
+}
+
+// --- info box for rail stations items
+
+function infoRailStation (item)
+{
+    var address = [item.entity ['main-road'], item.entity ['side-road']].join (', ');  // sometime side-road is blank
+
+    return '<h3>' + item.name + '</h3>' +
+           "<p style='font-size: 1.1em;' >" + address + '</p>' +
+           "<a href='" + item.entity ['live-info'] + "'>Live Station Board</a>";
+}
+
+// --- the os maps entry point
+
+function initialise ()
+{
+    var selected = null;
+    var entity   = null;
+    var infobox  = null;
+    var size     = null;
+
+    if (window.location.search == '?rail-station')
+    {
+        selected = document.getElementById ('rail-station');
+        entity   = 'rail-station';
+        infobox  =  infoRailStation;
+        size     = { w: 225, h: 200};
+    }
+    else if (window.location.search == '?history')
+    {
+        selected = document.getElementById ('history');
+        entity   = 'history';
+        infobox  =  infoHistory;
+        size     = { w: 500, h: 450};
+    }
+    else  // default to blue-plaque
+    {
+        selected = document.getElementById ('blue-plaque');
+        entity   = 'blue-plaque';
+        infobox  =  infoBluePlaque;
+        size     = { w: 450, h: 300};
+    }
+
+    selected.className += ' selected';  // changes appearance of selected
+
+    var url = API_ENDPOINT + entity;
+    var map = L.map ('map').setView ([MANCHESTER.center.lat, MANCHESTER.center.lng], MANCHESTER.zoom);
+
+    L.tileLayer ('https://api2.ordnancesurvey.co.uk/mapping_api/v1/service/zxy/EPSG%3A3857/Outdoor%203857/{z}/{x}/{y}.png?key=YOUR-KEY-HERE').addTo (map);
+
+    httpGet (url, function (entities)
+    {
+        entities.forEach (function (entity)
+        {
+            httpGet (entity.id, function (items)
+            {
+                var item = items [0];
+
+                if (item.loc != null && item.loc.type == 'Point')  // we only handle geojson points
+                {
+                    L.marker ([item.loc.coordinates [1], item.loc.coordinates [0]])
+                        .bindPopup (infobox (item))
+                        .addTo (map);
+                }
+            });
+        });
+    });
+}
+
+// --- start when document is all loaded
+
+document.onreadystatechange = function ()
+{
+   if (document.readyState == 'complete')
+   {
+       initialise ();
+   }
+}
